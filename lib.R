b4af30e026297d3505e628d7500bc6f2ab31df13
Shu Yan <yanshu.usc@gmail.com>
2017-01-19 02:12:01 -0500

    feature reduction

--- lib.R
+++ lib.R
@@ -23,60 +23,50 @@ heat_map <- function(data, xvar, yvar, colvar,
 }
 
 # date converting functions
+
+NA_VAL <- -1
 toDay <- function(x) {
-  sapply(x, function(y) {
+  as.numeric(Map(function(y) {
     age <- unlist(strsplit(y, split = " "))
     if(grepl("day", age[2])) 
       return(as.numeric(age[1]))
     else 
-      return(NA)
-  })
+      return(NA_VAL)
+  }, x))
 }
 
 toWeek <- function(x) {
-  sapply(x, function(x) {
+  as.numeric(Map(function(x) {
     age <- unlist(strsplit(x, split = " "))
     if(grepl("day", age[2])) 
       return(0)
     else if(grepl("week", age[2])) 
       return(as.numeric(age[1]))
     else 
-      return(NA)
-  })
+      return(NA_VAL)
+  }, x))
 }
 
 toMonth <- function(x) {
-  sapply(x, function(x) {
+  as.numeric(Map(function(x) {
     age <- unlist(strsplit(x, split = " "))
     if(grepl("day", age[2]) | grepl("week", age[2])) 
       return(0)
     else if(grepl("month", age[2])) 
       return(as.numeric(age[1]))
-    else return(NA)
-  })
+    else return(NA_VAL)
+  }, x))
 }
 
 toYear <- function(x) {
-  sapply(x, function(x) {
+  as.numeric(Map(function(x) {
     age <- unlist(strsplit(x, split = " "))
     if(grepl("day", age[2]) | grepl("week", age[2]) | grepl("month", age[2])) 
       return(0)
     else if(grepl("year", age[2])) 
       return(as.numeric(age[1]))
-    else return(NA)
-  })
-}
-
-
-toYear <- function(x) {
-  Map(function(x) {
-    age <- unlist(strsplit(x, split = " "))
-    if(grepl("day", age[2]) | grepl("week", age[2]) | grepl("month", age[2])) 
-      return(0)
-    else if(grepl("year", age[2])) 
-      return(as.numeric(age[1]))
-    else return(-1)
-  }, x)
+    else return(NA_VAL)
+  }, x))
 }
 
 featEng <- function(.data) {
@@ -84,24 +74,38 @@ featEng <- function(.data) {
             AnimalType = AnimalType,
             hasName = ifelse(is.na(Name), 0, 1),
             sex = ifelse(grepl("Female", SexuponOutcome), 0,
-                         ifelse(grepl("Male", SexuponOutcome), 1, NA)),
-            isNeutered = ifelse(grepl("Unknown", SexuponOutcome), NA, 
+                         ifelse(grepl("Male", SexuponOutcome), 1, NA_VAL)),
+            isNeutered = ifelse(grepl("Unknown", SexuponOutcome), NA_VAL,
                                 ifelse(grepl("Intact", SexuponOutcome), 0, 1)),
             isMix = ifelse(grepl(" Mix", Breed), 1, 0),
-            breed_pri = ifelse(grepl("/", Breed), 
-                               gsub("/.*", "", Breed), 
+            breed_pri = ifelse(grepl("/", Breed),
+                               gsub("/.*", "", Breed),
                                sub(" Mix", "", Breed)),
-            breed_sec = ifelse(grepl("/", Breed), 
-                               gsub(".*/", "", sub(" Mix", "", Breed)), 
+            breed_sec = ifelse(grepl("/", Breed),
+                               gsub(".*/", "", sub(" Mix", "", Breed)),
                                sub(" Mix", "", Breed)),
-            pattern_pri = ifelse(grepl("/", Color), 
-                                 gsub("/.*", "", Color), 
+            pattern_pri = ifelse(grepl("/", Color),
+                                 gsub("/.*", "", Color),
                                  Color),
-            pattern_sec = ifelse(grepl("/", Color), 
-                                 gsub(".*/", "", Color), 
+            pattern_sec = ifelse(grepl("/", Color),
+                                 gsub(".*/", "", Color),
                                  Color),
             ageInDay = toDay(AgeuponOutcome),
             ageInWeek = toWeek(AgeuponOutcome),
             ageInMonth = toMonth(AgeuponOutcome),
-            ageInYear = toYear(AgeuponOutcome))
+            ageInYear = toYear(AgeuponOutcome),
+            outcomeYear = year(strftime(DateTime)) - 2e3,
+            outcomeMonth = month(strftime(DateTime)),
+            outcomeMday = mday(strftime(DateTime)),
+            outcomeWday = wday(strftime(DateTime)),
+            outcomeHour = hour(strftime(DateTime)))
 }
+
+to_factor <- function(x, levels = NULL) {
+  if(is.null(levels))
+    levels <- sort(unique(x))
+  factor(x, levels = levels)
+}
+
+# information gain
+# entropy::entropy(animal_train$OutcomeType)
--- preProc.R
+++ preProc.R
@@ -1,25 +1,57 @@
+## load library
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(caret)
 library(data.table)
 
+## set global environment
 options(stringsAsFactors = FALSE)
-## set wording directory
-setwd("~/Workspace/shelterAnimals/")
 rm(list = ls())
+
+## set wording directory
+setwd("~/Workspace/shelter_animals/")
 source("./lib.R")
 
 ## load data
 animal_train <- fread("./data/train.csv", na.strings = "")
 animal_test <- fread("./data/test.csv", na.strings = "")
+
+## create features
 train_feature <- animal_train %>% featEng()
-train_target <- animal_train$OutcomeType
+train_target <- as.factor(animal_train$OutcomeType)
 test_feature <- animal_test %>% featEng()
 
+## convert character to factor
+for(x in names(train_feature)) {
+  if(class(train_feature[,x]) == "character") {
+    cat("Converting", x, "to factor...\n")
+    train_feature[,x] <- to_factor(train_feature[,x])
+    test_feature[,x] <- to_factor(test_feature[,x], 
+                                  levels = levels(train_feature[,x]))
+  }
+}
+
+## create dummy variables
+train_dummy <- dummyVars(~., train_feature)
+train_dummy <- model.matrix(~. -1, data = train_feature, )
 
 library(doMC)
-library(parallel)
 registerDoMC(cores = detectCores())
+
+## feature selection
+ctrl <- rfeControl(functions = rfFuncs,
+                   method = "repeatedcv",
+                   repeats = 3,
+                   verbose = TRUE)
+
+lmProfile <- rfe(train_dummy,
+                 train_target,
+                 sizes = 1:length(train_dummy),
+                 rfeControl = ctrl)
+
+
+
 
 library(caret)
 library(pROC)
